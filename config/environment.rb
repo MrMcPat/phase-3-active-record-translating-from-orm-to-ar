39f4554ddde287f37f72a5b9f962f994aaedcd7e
Patrick Liang <pl204511194@yahoo.com>
2022-02-25 11:33:09 -0500

    commit tree 682a148fefa8770a954bf24789a75a06aef6aa4a

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,16 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+binding.pry
+""
